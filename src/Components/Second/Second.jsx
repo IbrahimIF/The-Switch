e11d1fd0345c9f4b15d47e8245bad8e027faef0a
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2023-09-13 21:22:05 +0100

    fixed the show boxes on main switch and begun second page

--- src/Components/Second/Second.jsx
+++ src/Components/Second/Second.jsx
@@ -3,10 +3,13 @@ import './Second.css'
 function Second() {
 
   return (
-
 <>
 <div>
-    
+<div className="switches">
+  <div className="shadowes"/>
+      <input type="checkbox"/>
+      <label></label>
+  </div> 
 </div>
 </>
   )
